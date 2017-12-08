85f22db154a4847b80d39f4a613ae5ae50ef48b8
Chibuikem Amaechi <chibuikem_amaechi@Chibuikems-MacBook-Pro.local>
2017-12-08 11:46:58 -0600

    Modified .main.cpp.swp and main.cpp

--- main.cpp
+++ main.cpp
@@ -19,61 +19,61 @@ void output(double output1, double output2, char& output3);
 
 int main()
 {
-	double m, cM, f, i;
-	char ans;
+    double m, cM, f, i;
+    char ans;
 
-	do
-	{
-		input(f,i);
-		convertLength(f, i, m, cM);
-		output(m, cM, ans);
-		
-		if(ans == 'y' || ans == 'Y')
-			cout<<"\n\n";
-	}while(ans == 'y' || ans == 'Y');
-	
-	return 0;
+    do
+    {
+        input(f,i);
+        convertLength(f, i, m, cM);
+        output(m, cM, ans);
+
+        if(ans == 'y' || ans == 'Y')
+            cout<<"\n\n";
+    }while(ans == 'y' || ans == 'Y');
+
+    return 0;
 }
 
 void input(double& input1, double& input2)
 {
-	char a;
-	cout<<"Enter in length in feet and inches: ";
-	cin>>input1>>a>>input2;
-	cout<<endl;
+    char a;
+    cout<<"Enter in length in feet and inches: ";
+    cin>>input1>>a>>input2;
+    cout<<endl;
 }
 
 void convertLength(double& feet, double inches, double& meters, double& centimeters)
 {
-	if (inches < 12)
-	{
-		feet+=(inches/12);
-		meters = 0.3048*feet;
-		centimeters = 100*meters;
-	}
-	else
-	{
-		feet+=1;
-		meters = 0.3048*feet;
-		centimeters = 100*meters;
-	}
+    if (inches < 12)
+    {
+        feet+=(inches/12);
+        meters = 0.3048*feet;
+        centimeters = 100*meters;
+    }
+    else
+    {
+        feet+=1;
+        meters = 0.3048*feet;
+        centimeters = 100*meters;
+    }
 }
 
 void output(double output1, double output2, char& output3)
 {
-	cout.setf(ios::showpoint);
-	cout.setf(ios::fixed);
-	cout.precision(2);
+    cout.setf(ios::showpoint);
+    cout.setf(ios::fixed);
+    cout.precision(2);
 
-	cout<<output1<<" meters\n\n";
-	cout<<output2<<" centimeters\n\n";
-	cout<<"Start over? (y/n): ";
-	cin>>output3;
+    cout<<output1<<" meters\n\n";
+    cout<<output2<<" centimeters\n\n";
+    cout<<"Start over? (y/n): ";
+    cin>>output3;
 }
 
 /*
-void clearScreen()
-{
-	cout<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
-}
-*/
+   void clearScreen()
+   {
+   cout<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+   }
+   */
